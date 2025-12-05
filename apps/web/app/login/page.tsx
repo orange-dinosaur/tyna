29686c9f3428d2a2d1c3d986ca1a0cf80636d4d8
orange-dinosaur <user@randomprojects.xyz>
2025-12-05 21:43:42 +0100

    Add updated app logo

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,8 +1,17 @@
 import { LoginForm } from '@/components/login-form';
+import { images } from '@workspace/assets';
+import Image from 'next/image';
 
 export default function LoginPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <Image
+                src={images.logo}
+                alt="App Logo"
+                width={50}
+                height={50}
+                className="w-auto"
+            />
             <div className="flex w-full max-w-sm flex-col gap-6">
                 <LoginForm />
             </div>
