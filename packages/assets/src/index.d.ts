d1c7a28bd57fefa82f33f679b7691a72913e13e4
orange-dinosaur <user@randomprojects.xyz>
2025-12-05 21:43:29 +0100

    Add package to manage shared assets

---
+++ packages/assets/src/index.d.ts
@@ -0,0 +1,12 @@
+export declare const images: {
+    logo: string;
+};
+
+export declare const icons: {};
+
+declare const assets: {
+    images: typeof images;
+    icons: typeof icons;
+};
+
+export default assets;
---
+++ packages/assets/src/index.ts
@@ -0,0 +1,10 @@
+import logo from './images/logo.png';
+
+export const images = {
+    logo,
+};
+
+export const icons = {};
+
+const assets = { images, icons };
+export default assets;
