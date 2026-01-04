f925096c9e0e606aa686500794f3d30ab371d76c
orange-dinosaur <user@randomprojects.xyz>
2026-01-04 16:07:45 +0100

    Remove biolerplate code

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from '@workspace/ui-web/components/button';
-
 export default function Page() {
     return (
         <div className="flex items-center justify-center min-h-svh">
             <div className="flex flex-col items-center justify-center gap-4">
-                <h1 className="text-2xl font-bold">Hello World</h1>
-                <Button size="sm">Button</Button>
+                <h1 className="text-2xl font-bold">
+                    Welcome to {process.env.NEXT_PUBLIC_APP_NAME}
+                </h1>
             </div>
         </div>
     );
