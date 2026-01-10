44981a65b46636ab0cc4697f261b5073151df0b7
orange-dinosaur <user@randomprojects.xyz>
2026-01-10 18:38:41 +0100

    Implement sidebar layout for protected routes and add new UI components

--- apps/web/app/(protected)/layout.tsx
+++ apps/web/app/(protected)/layout.tsx
@@ -1,18 +1,27 @@
+import { AppSidebar } from '@/components/app-sidebar';
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from '@workspace/ui-web/components/sidebar';
+
 export default function ProtectedLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <div className="flex flex-col items-center justify-start min-h-svh">
-            <div className="bg-muted flex items-center justify-start gap-4 w-full h-24">
-                <h1>Protected Layout</h1>
-                <h1>Page 1</h1>
-                <h1>Page 2</h1>
-            </div>
-            <div className="flex items-center justify-center w-full overflow-x-hidden flex-1 bg-gray-600">
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                    <div className="flex items-center gap-2 px-4">
+                        <SidebarTrigger className="-ml-1" />
+                    </div>
+                </header>
+
                 {children}
-            </div>
-        </div>
+            </SidebarInset>
+        </SidebarProvider>
     );
 }
